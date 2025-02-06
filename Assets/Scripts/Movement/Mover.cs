e41993772b07c17707413444190eb19c3f4ecd08
viturv-kevadiya <viturv@yyzstudio.com>
2025-02-07 01:28:45 +0530

    Lecture 82: Saveable component and Gam objescts

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,11 +1,12 @@
 using RPG.Core;
 using UnityEngine;
 using UnityEngine.AI;
+using RPG.Saving;
 
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour , IAction
+    public class Mover : MonoBehaviour , IAction, ISaveable
     {
 
         Transform target;
@@ -54,6 +55,18 @@ namespace RPG.Movement
             GetComponent<Animator>().SetFloat("ForwardSpeed",Speed);
         }
 
+        public object CaptureState()
+        {
+            return new SerializableVector3( transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            SerializableVector3 position = (SerializableVector3)state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position= position.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
+        }
     }
 
 }
